56a25a814d80707ea631a09d42a26cf6c85d6ef5
Robert Schrom <robertschrom2@gmail.com>
2017-04-12 14:52:20 -0400

    Update plot.py

--- improve_matplot/plot.py
+++ improve_matplot/plot.py
@@ -2,7 +2,7 @@ import matplotlib as mpl
 mpl.use('Agg')
 
 # use helvetica instead of default font
-def adjustFonts(mpl):
+def adjust_fonts(mpl):
     mpl.rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
     mpl.rc('text', usetex=True)
     return
