abeda081dc2cc50b1119b578185c7cc11fe5387d
Robert Schrom <snowwxradar@users.noreply.github.com>
2017-04-12 19:05:46 -0400

    Fixed scaling issues with figure size

--- improve_matplot/panel.py
+++ improve_matplot/panel.py
@@ -12,8 +12,8 @@ class Panel:
         self.colorbar = cb
         self.xbounds = []
         self.ybounds = []
-        self.txscale = 105.56
-        self.lbscale = 135.71
+        self.txscale = 16./800.
+        self.lbscale = 11./800.
         self.panel_ind = 3
 
     # set methods
@@ -37,8 +37,10 @@ class Panel:
     def get_font_sizes(self):
         fpix = self.figure.get_size_inches()*self.figure.dpi
 
-        txfontsize = fpix[1]/self.txscale
-        lbfontsize = fpix[1]/self.lbscale
+        print fpix
+        txfontsize = fpix[0]*self.txscale
+        lbfontsize = fpix[0]*self.lbscale
+        print txfontsize, lbfontsize
         return txfontsize, lbfontsize
 
     # make axis look tolerable
